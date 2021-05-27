4d777272eb076fbd7e447b6586daba5137218589
Calvo <francocalvo@tutanota.com>
2021-05-27 13:14:54 -0300

    Install oh-my-zsh

---
+++ .screenlayout/amdr9layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off --output DVI-D-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off

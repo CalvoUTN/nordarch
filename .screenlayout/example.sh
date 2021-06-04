3aa63697c8b2a03d1d692e8b7f86417b9245ca14
Calvo <francocalvo@tutanota.com>
2021-06-04 16:58:31 -0300

    Add new screen layout for AMD GPU

--- .screenlayout/example.sh
+++ .screenlayout/example.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-5 --off
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off --output DVI-D-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
